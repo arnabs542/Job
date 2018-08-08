254d0664c1f2d0afdc9d1f41e84b11d907bb8cff
timchen <timchen101008@gmail.com>
2018-08-08 01:26:03 -0400

    Checkin

--- LeetCode/LeetCode.java
+++ LeetCode/LeetCode.java
@@ -3092,9 +3092,21 @@
   - Matain prefix int[]
   # String
 
+529. Minesweeper
+  - DFS
 
-
-
+540. Single Element in a Sorted Array
+  - Find the single element in a sorted array that include pair elements and one single element. 
+  -  public static int singleNonDuplicate(int[] nums) {
+          int start = 0, end = nums.length - 1;
+          while (start < end) {
+              int mid = (start + end) / 2;
+              if (mid % 2 == 1) mid--;
+              if (nums[mid] != nums[mid + 1]) end = mid; else start = mid + 2;
+          }
+          return nums[start];
+      }
+  - Binary Search
 
 
 

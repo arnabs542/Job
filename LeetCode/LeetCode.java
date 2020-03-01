8c5518d84c4d21b1868ad67859db7aa82f1285d5
Tingyu Chen <chentingyu1008@yahoo.com>
2020-03-01 16:55:15 -0500

    Update

--- LeetCode/LeetCode.java
+++ LeetCode/LeetCode.java
@@ -83,12 +83,12 @@
         int max = 0;
         String str="";
 
-        for(int j =0;j<s.length();j++) {
-            for(int i = 0;i<=j;i++) {
-                dp[i][j] = s.charAt(i) == s.charAt(j) && (j-i<=2 || dp[i+1][j-1]);
-                if(dp[i][j] && j-i+1>max) {
-                    max = j-i;
-                    str = s.substring(i, j+1);
+        for(int i =0;i<s.length();i++) {
+            for(int j=0;j<=i;j++) {
+                dp[i][j] = s.charAt(i) == s.charAt(j) && (i-j<=2 || dp[i-1][j+1]);
+                if(dp[i][j] && i-j+1>max) {
+                    max = i-j+1;
+                    res = s.substring(j, i+1);
                 }
             }
         }
